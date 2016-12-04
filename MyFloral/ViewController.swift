fd18aef45347a4be639d1b059931c88590d70807
zhimingcow <zhimingcow@gmail.com>
2016-12-04 14:42:23 +0800

    Initial Commit

---
+++ MyFloral/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFloral
+//
+//  Created by cow on 16/12/4.
+//  Copyright © 2016年 cow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MyFloralTests/MyFloralTests.swift
@@ -0,0 +1,36 @@
+//
+//  MyFloralTests.swift
+//  MyFloralTests
+//
+//  Created by cow on 16/12/4.
+//  Copyright © 2016年 cow. All rights reserved.
+//
+
+import XCTest
+@testable import MyFloral
+
+class MyFloralTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
